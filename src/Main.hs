1b2320dd487fc7c139a8091a406a831289c0a86d
George Magafossis <george.magafossis@aisera.com>
2025-02-19 13:30:43 +0200

    Experiment with cabal executables

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,8 @@
 module Main (main) where
 
-import Lexer.Lexer
 import Parser.Parser
-
-testLexer :: String -> IO ()
-testLexer s = do
-  let res = lexer s
-  case res of
-    Left err -> putStrLn err
-    Right tokens -> mapM_ (\token -> putStrLn $ "Token: " ++ (show token)) tokens
-
 
 main :: IO ()
 main = do
   s <- getContents
-  testLexer s
   putStrLn $ show (test s)
